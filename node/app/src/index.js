7b73bfbcadeb864860cc4eb92b934e004c0e28b0
Alexandre Correa <alexcorr@yahoo.com>
2022-02-28 13:43:20 -0300

    Limpando o repositório

--- node/app/src/index.js
+++ node/app/src/index.js
@@ -11,7 +11,10 @@ const config = {
     database: 'nodedb'
 };
 
+
 const mysql2 = require('mysql2/promise')
+
+appInit();
 
 app.get('/', async (req,res) => {
     res.send(buildResponse(await getPeopleNames()));
@@ -20,6 +23,7 @@ app.get('/', async (req,res) => {
 app.listen(port, () => {
     console.log('Running on port  ' + port);
 })
+
 
 function buildResponse(peopleNames) {
     var htmlResult = '<h1>Full Cycle Rocks featuring Node, MySql and Nginx</h1>';
@@ -36,11 +40,6 @@ async function getPeopleNames() {
     const sql = "SELECT * from people";
     var [rows, cols] = await runSqlQuery(sql);
 
-    if (rows.length === 0) {
-        await insertPeople();
-        [rows, cols] = await runSqlQuery(sql);
-    }
-
     Object.keys(rows).forEach(function(key) {
         peopleNames.push(rows[key].name);
     })    
@@ -48,14 +47,58 @@ async function getPeopleNames() {
     return peopleNames;
 }
 
+
+async function appInit() {
+    await assertDatabaseOK();
+    await assertSchemaOK();
+    await assertDatabaseIsPopulated();
+}
+
+async function assertDatabaseOK() {
+    const configAdmin = {
+        host: 'db',
+        user: 'root',
+        password: 'root'
+    };
+    const sql = "CREATE DATABASE IF NOT EXISTS nodedb"
+    
+    const connection = await mysql2.createConnection(configAdmin);
+    await connection.query(sql);
+    connection.end()
+}
+
+async function assertSchemaOK() {
+    const sql = "SELECT count(*) as tablesCount \
+                 FROM information_schema.TABLES \
+                 WHERE (TABLE_SCHEMA = 'nodedb') AND (TABLE_NAME = 'people')"
+
+    var [rows, cols] = await runSqlQuery(sql)
+    if (rows[0].tablesCount === 0) {
+        await createSchema()
+    }
+}
+
+async function createSchema() {
+    const sql = "CREATE TABLE people (id int not null auto_increment, name varchar(255), primary key(id))"
+    await executeSql(sql);
+}
+
+async function assertDatabaseIsPopulated() {
+    var peopleNames = await getPeopleNames();
+    if (peopleNames.length === 0) {
+        await insertPeople();
+    }
+}
+
 async function insertPeople() {
     await insertPerson('Alex')
     await insertPerson('Wesley')
     await insertPerson('Vini Malvadeza')
+    await insertPerson('Messi')
 }
 
 async function insertPerson(name) {
-    await runInsertSql(util.format("INSERT INTO people(name) values('%s')", name));
+    await executeSql(util.format("INSERT INTO people(name) values('%s')", name));
 }
 
 async function runSqlQuery(sql) {
@@ -65,10 +108,10 @@ async function runSqlQuery(sql) {
     return [rows, cols];
 }
 
-async function runInsertSql(sql) {
+async function executeSql(sql) {
     const connection = await mysql2.createConnection(config);
     await connection.query(sql);
     connection.end()
-
 }
+
 
